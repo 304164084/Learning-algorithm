f4139b4deea2730a7b19e30256af849a008eb7c6
304164084 <17665111455@sina.cn>
2020-05-19 16:15:21 +0800

    添加快速排序函数

--- Learning algorithm/main.swift
+++ Learning algorithm/main.swift
@@ -24,3 +24,37 @@ func callWeekCompetition(_ number: Int) -> () {
 
 callWeekCompetition(188)
 
+
+// MARK: 快速排序O(nlogn)
+
+func quickSort(_ array: [Int]) -> [Int] {
+    guard array.count > 1 else {
+        return array
+    }
+    
+   // var low = [Int]()
+   // var high = [Int]()
+   
+   /*    
+    for item in array {
+        if item > array[0] {
+            high.append(item)
+        } else {
+            low.append(item)
+        }
+    }
+   */
+    // 随机取递归条件，取的点最好可以均分数组
+    let pivot = array[0]
+    let low = array.filter{$0 < pivot}
+    let equal = array.filter{$0 == pivot}
+    let high = array.filter{$0 > pivot}
+    let result = quickSort(low) + equal + quickSort(high)
+    
+    print(result)
+    return result
+}
+
+let list = [ 10, 0, 3, 9, 2, 14, 8, 27, 1, 5, 8, -1, 26 ]
+quickSort(list)
+
