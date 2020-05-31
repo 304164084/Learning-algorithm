865067db46b4e4c3c49a4a218cac715fb3f94c63
banglin <17665111455@sina.cn>
2020-05-31 22:10:23 +0800

    FIX - 先提交几个类

--- Learning algorithm/test.swift
+++ Learning algorithm/test.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+ 
+class bbb {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return isSameTree(root?.left, root?.right)
+    }
+
+    func isSameTree(_ t1: TreeNode?, _ t2: TreeNode?) -> Bool {
+        if t1 == nil && t2 == nil {
+            return true
+        }
+        if t1 == nil || t2 == nil {
+            return false
+        }
+
+        return (t1?.val == t2?.val) && isSameTree(t1?.left, t2?.right) && isSameTree(t1?.right, t2?.left)
+    }
+}
